533e3bbd9ab87e7e74a3515a6cffea62565b95bd
Engel <engelbertforson@gmail.com>
2024-01-12 05:19:21 -0800

    called Display class

---
+++ src/sample/Display.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Display{
+
+	public void display(){
+
+		System.out.println("I'm coming from the display method in the display class");
+	}
+
+}
+
+
+
+
---
+++ src/sample/Main.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Main {
+
+
+    public static void main(String args[]){
+
+        System.out.println("Hello world");
+
+	     Display display = new Display();
+
+	     display.display();
+
+    }
+
+    
+}
